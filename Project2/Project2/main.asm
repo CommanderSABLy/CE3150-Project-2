2d7b43fc54b1a84f10ea3a29ac4e2b40552f0ddb
spmcenaney <spmcenaney@outlook.com>
2020-05-09 22:02:18 -0500

    created project

---
+++ Project2/Project2/main.asm
@@ -0,0 +1,12 @@
+;
+; Project2.asm
+;
+; Created: 5/9/2020 10:01:08 PM
+; Author : Saul Lynn, Shane McEnaney, Pamela Petterchak, Adam Sawyer
+;
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
