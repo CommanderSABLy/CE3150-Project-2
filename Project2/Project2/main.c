75b4972fdb656fac14de6aba2438cc4617454a6e
spmcenaney <spmcenaney@outlook.com>
2020-05-09 22:29:39 -0500

    changed to c project

---
+++ Project2/Project2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project2.c
+ *
+ * Created: 5/9/2020 10:28:50 PM
+ * Author : Saul Lynn, Shane McEnaney, Pamela Petterchak, Adam Sawyer
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
