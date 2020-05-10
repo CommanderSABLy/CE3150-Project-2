59404ebdddfad0a9c4614cb25dddc99e12c788db
spmcenaney <spmcenaney@outlook.com>
2020-05-10 03:11:43 -0500

    implemented timer

--- Project2/Project2/main.c
+++ Project2/Project2/main.c
@@ -9,6 +9,8 @@
 #include <avr/interrupt.h>
 #include <avr/sfr_defs.h> // bit_is_clear(PIND, PD2)
 
+void timer();
+
 ISR (TIMER0_OVF_vect);
 
 #define TOT_ITERATIONS 25
@@ -24,6 +26,8 @@ int mode = 0;
 int main() {
 	DDRD = 0xFF;
 	PORTD = 0xFF;
+	DDRE = 0xBF;
+	PORTE |= (1<<5); // off
 	DDRA = 0x00; // make PA input
 	PORTA = 0xFF; // enable pull up on PA
 	
@@ -44,6 +48,33 @@ int main() {
 			mode = 0;
 		} else if (bit_is_clear(PINA, PA2)) {
 			mode = -1;
+		} else if (bit_is_clear(PINE, PE6)) {
+			mode = -1;
+			while (bit_is_clear(PINE, PE6)) {}
+			timer();
+		}
+	}
+}
+
+void timer() {
+	PORTE &= ~(1<<5); // on
+	mode = 0;
+	while (!bit_is_clear(PINE, PE6)) {
+		PORTD = PINA;
+	}
+	while (bit_is_clear(PINE, PE6)) {}
+	while(1) {
+		if (bit_is_clear(PINA, PA0)) {
+			mode = 2;
+		} else if (bit_is_clear(PINA, PA1)) {
+			mode = 0;
+		} else if (bit_is_clear(PINA, PA2)) {
+			mode = -1;
+		} else if (bit_is_clear(PINE, PE6)) {
+			PORTE |= (1<<5); // off
+			mode = -1;
+			while (bit_is_clear(PINE, PE6)) {}
+				return;
 		}
 	}
 }
@@ -64,17 +95,20 @@ ISR (TIMER0_OVF_vect) {
 				}
 			}
 		} else if (mode == 2) {
-			tenth--;
-			if (tenth == -1) {
+			if (tenth == 0) {
 				tenth = 9;
-				ones--;
-				if (ones == -1) {
+				if (ones == 0) {
 					ones = 9;
+				} else {
+					ones--;
 				}
+			} else {
+				tenth--;
 			}
 		} else if (mode == -1) {
 			tenth = 0;
 			ones = 0;
+			PORTD = 0xFF;
 		}
 		PORTD ^= (ones*16 + tenth);
 		iterations = 0;
